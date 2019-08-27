da656182def23407a56ab510157004adf0ab1f69
qwerkh <narongsao98@gmail.com>
2019-08-27 21:33:49 +0700

    update meteor to 1.8.1

--- node_modules/element-ui/packages/date-picker/src/basic/time-spinner.vue
+++ node_modules/element-ui/packages/date-picker/src/basic/time-spinner.vue
@@ -14,6 +14,7 @@
           @click="handleClick('hours', { value: hour, disabled: disabled })"
           v-for="(disabled, hour) in hoursList"
           class="el-time-spinner__item"
+          :key="hour"
           :class="{ 'active': hour === hours, 'disabled': disabled }">{{ ('0' + (amPmMode ? (hour % 12 || 12) : hour )).slice(-2) }}{{ amPm(hour) }}</li>
       </el-scrollbar>
       <el-scrollbar
@@ -28,6 +29,7 @@
         <li
           @click="handleClick('minutes', { value: key, disabled: false })"
           v-for="(enabled, key) in minutesList"
+          :key="key"
           class="el-time-spinner__item"
           :class="{ 'active': key === minutes, disabled: !enabled }">{{ ('0' + key).slice(-2) }}</li>
       </el-scrollbar>
@@ -99,7 +101,7 @@
 </template>
 
 <script type="text/babel">
-  import { getRangeHours, getRangeMinutes, modifyTime } from '../util';
+  import { getRangeHours, getRangeMinutes, modifyTime } from 'element-ui/src/utils/date-util';
   import ElScrollbar from 'element-ui/packages/scrollbar';
   import RepeatClick from 'element-ui/src/directives/repeat-click';
 

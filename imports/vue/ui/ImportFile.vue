2dfacd39f3013c3710e8407d4bccc8963ced657a
qwerkh <narongsao98@gmail.com>
2020-02-14 17:40:36 +0700

    fix upload file

--- imports/vue/ui/ImportFile.vue
+++ imports/vue/ui/ImportFile.vue
@@ -38,11 +38,13 @@
                 class="upload-demo"
                 drag
                 v-model="testImport.fileName"
-                action="https://jsonplaceholder.typicode.com/posts/"
+                action="ok"
                 :on-preview="handlePreview"
                 :on-success="handleSuccess"
                 :before-upload="handleBeforeUpload"
         >
+            <!--action="https://jsonplaceholder.typicode.com/posts/"-->
+
             <i class="el-icon-upload"></i>
             <div class="el-upload__text">Drop file here or <em>click to upload</em></div>
             <div class="el-upload__tip" slot="tip">files with a size less than 500kb</div>

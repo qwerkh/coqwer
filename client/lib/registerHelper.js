3a499bbfe0e815e9bc88bbf80d06822891d8936a
qwerkh <narongsao98@gmail.com>
2020-11-07 23:25:11 +0700

    update size mini invoice

--- client/lib/registerHelper.js
+++ client/lib/registerHelper.js
@@ -11,3 +11,12 @@ Template.registerHelper('formatNumber', (dataNumber) => {
     }
     return 0;
 });
+
+Template.registerHelper('seperateNumber', (val) => {
+    if (val !== "" && val !== undefined && val !== 0 && val !== '0' && val !== null) {
+        val = numeral(val).value() + "";
+        let newVal = val.split(".");
+        return newVal[0].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + (!!newVal[1] ? "." + newVal[1] : "");
+    }
+    return 0;
+});

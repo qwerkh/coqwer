510a905a874b3eb9b7fd649f7d13a404a0366ec6
qwerkh <narongsao98@gmail.com>
2022-12-13 20:13:40 +0700

    d

--- client/lib/registerHelper.js
+++ client/lib/registerHelper.js
@@ -1,3 +1,5 @@
+import {GeneralFunction} from "../../imports/api/methods/generalFunction";
+
 Template.registerHelper('formatDate', (date) => {
 
     if (date) {
@@ -10,6 +12,24 @@ Template.registerHelper('formatNumber', (dataNumber) => {
         return numeral(dataNumber).format("0,00.000");
     }
     return 0;
+});
+Template.registerHelper('exchangeFn', (amount, curTo) => {
+    let val = 0;
+    if (curTo === "USD") {
+        val = GeneralFunction.exchange(Session.get("baseCurrency"), curTo, amount);
+
+    } else if (curTo === "KHR") {
+        val = GeneralFunction.exchange(Session.get("baseCurrency"), curTo, amount);
+
+    } else if (curTo === "THB") {
+        val = GeneralFunction.exchange(Session.get("baseCurrency"), curTo, amount);
+    } else {
+        val = GeneralFunction.exchange(Session.get("baseCurrency"), curTo, amount);
+    }
+    val = math.round(val * 100) / 100;
+    val = numeral(val).value() + "";
+    let newVal = val.split(".");
+    return newVal[0].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + (!!newVal[1] ? "." + newVal[1] : "");
 });
 
 Template.registerHelper('seperateNumber', (val) => {
